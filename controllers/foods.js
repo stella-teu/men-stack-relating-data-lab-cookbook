9e296b613a92d29050241d17b09618a06f259bde
StellaTeuscher <stella.teuscher@gmail.com>
2025-03-29 17:27:05 -0400

    succesfully added new food to db

--- controllers/foods.js
+++ controllers/foods.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 const router = Router();
-import User from "./auth.js";
+import User from "../models/user.js";
 
 router.get("/", async (req, res) => {
     res.render('foods/index.ejs');
@@ -8,6 +8,18 @@ router.get("/", async (req, res) => {
 
 router.get("/new", (req, res) => {
     res.render("foods/new.ejs");
+})
+
+router.post("/", async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.pantry.push(req.body);
+        await currentUser.save();
+        res.redirect("/users/"+req.session.user._id+"/foods");
+    } catch (error){
+        console.log(error);
+        res.redirect("/");
+    }
 })
 
 export default router;
